beebfc3c639d33fee3302145a1c265609d5dfbca
Joey574 <joeysoroka@outlook.com>
2026-08-21 11:10:35 -0700

    fix3

--- modules/test-node.nix
+++ modules/test-node.nix
@@ -8,6 +8,7 @@
   nix.settings.trusted-users = [ "root" "admin" ];
 
   users.users.admin = {
+    isNormalUser = true;
     extraGroups = [ "wheel" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ7M/4N/2mWT6hBWs9vkiRV/uixjfnYOG3MPhxk5/UHT testkey"
